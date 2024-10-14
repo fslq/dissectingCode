2b3f05d9316a10ff27fd05421aafdaacc97b1230
fslq <2296602826@qq.com>
2024-10-14 14:59:00 +0800

    1.3.4

--- src/java8/base/juc/mutithread/OwnThreadPool.java
+++ src/java8/base/juc/mutithread/OwnThreadPool.java
@@ -2,7 +2,6 @@ package java8.base.juc.mutithread;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import java.util.concurrent.TimeUnit;
 
@@ -22,9 +21,9 @@ public class OwnThreadPool {
 
     public static ThreadPoolExecutor getInstance(){
         //单例模式-双重检测锁
-        if(instance==null){
+        if(instance==null){//首次检查：在没有进入同步块之前进行第一次null检查。如果instance已经被初始化，那么多个线程可以直接返回已有的实例，而无需进入开销较大的同步块，从而提高了程序的性能。这对于频繁调用获取单例实例的场景非常重要，可以避免不必要的线程阻塞和同步开销。
             synchronized(OwnThreadPool.class){
-                if(instance==null){
+                if(instance==null){//第二次检查：在进入同步块后再次检查instance是否为null。这是因为可能有多个线程同时在首次检查时发现instance为null，然后都试图进入同步块。如果没有第二次检查，那么即使第一个进入同步块的线程已经初始化了instance，后续进入同步块的线程仍然会重复进行初始化操作。第二次检查确保只有在instance确实为null时才进行初始化，避免了重复初始化的问题
                     instance = new ThreadPoolExecutor(
                         corePoolSize,
                         maximumPoolSize, 
